4ad7246a9c3e025414b9cb09b2cd223bf819241d
pratik bhattarai <bhattaraipratik663@gmail.com>
2023-05-27 14:16:07 +0545

    27May

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+
+import React from "react";
+
+
+export default function Navbar() {
+
+    return(
+        <header className="bg-gray-800 md:sticky top-0 z-10">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a className="title-font font-medium text-white mb-4 md:mb-0">
+            <a href="#about" className="ml-3 text-xl">
+             Pratik Bhattarai
+            </a>
+          </a>
+          <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+            <a href="#projects" className="mr-5 hover:text-white text-gray-400">
+              Past Work
+            </a>
+            <a href="#skills" className="mr-5 hover:text-white text-gray-400">
+              Skills
+            </a>
+            <a href="#testimonials" className="mr-5 hover:text-white text-gray-400">
+              Testimonials
+            </a>
+          </nav>
+          <a
+            href="#contact"
+            className="inline-flex items-center bg-blue-600 border-0 py-1 px-3 focus:outline-none hover:bg-blue-700 rounded text-base mt-4 md:mt-0 text-white">
+            Hire Me   
+          </a>
+        </div>
+      </header>
+    )
+
+}
---
+++ src/data.js
@@ -0,0 +1,31 @@
+
+import gyankoopImg from "./image/gyankoop.png"
+import letsdrive from "./image/let'sDrive.png"
+import logistics from "./image/Admin.png"
+
+export const projects = [
+    {
+      title: "Gyankoop",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: gyankoopImg,
+      link: "https://lambent-gelato-dbf181.netlify.app/",
+    },
+
+    {
+      title: "Let's Drive",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: letsdrive,
+      link: "https://nimble-lebkuchen-55d69d.netlify.app/",
+    },
+
+    {
+      title: "365 Logistics Application",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: logistics,
+      link: "https://nimble-lebkuchen-55d69d.netlify.app/",
+    },
+
+];
